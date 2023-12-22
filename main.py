b1629be27a83eaca1ba7e798c14f7ad1a1aee919
Zhang-Wei Hong <zwhong@improbablex001.csail.mit.edu>
2023-12-22 05:06:49 -0500

    Update experiment setup

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import nltk
 import spacy
 import torch
-from nltk.corpus import stopwords
-from spacy.lang.en import English
 from diffusers import AutoPipelineForText2Image
 from diffusers import DiffusionPipeline
 from diffusers.utils import export_to_video
-from PIL import Image
 from moviepy.editor import VideoFileClip
+from nltk.corpus import stopwords
+from os import listdir
+from os.path import join
+from spacy.lang.en import English
+from tqdm import tqdm
+from PIL import Image
+
+SOURCE_FOLDER = './articles/'
+DEST_FOLDER = './outputs/'
+TXT_SUFFIX = '.txt'
 
 # Resize image to 1024x576
 IMAGE_WIDTH = 1024
@@ -16,7 +23,6 @@ IMAGE_HEIGHT = 576
 sp = spacy.load("en_core_web_sm")
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
-
 
 def split_sentences(text: str) -> [str]:
     # Load the English tokenizer from spaCy
@@ -108,39 +114,51 @@ def to_prompt(article: str) -> str:
             
     return general_prompt + ",".join(prompt)
 
-def generate_image(article:str, topic:str, pipeline_text2image):
-    prompt = to_prompt(article)
-    print(prompt)
+def generate_image(article:str, topic:str, pipeline_text2image, original:bool=True):
+    prompt = article
+    if not original:
+        prompt = to_prompt(article)
+        with open(f"{DEST_FOLDER}/{topic}_prompt.txt", 'w') as f:
+            print(prompt, file=f)
+    output_file = f"{DEST_FOLDER}/{'original' if original else ''}/{topic}.png"
 
-    pipeline_text2image.enable_model_cpu_offload()
     image = pipeline_text2image(prompt=prompt, guidance_scale=0.0, num_inference_steps=10, height=IMAGE_HEIGHT, width=IMAGE_WIDTH).images[0]
-    image.save(f"{topic}.png")
+    image.save(output_file)
 
-def generate_video(topic:str, pipeline_image2video):
-    image = Image.open(f"{topic}.png")
+def generate_video(topic:str, pipeline_image2video, original:bool=True):
+    image = Image.open(f"{DEST_FOLDER}/{'original' if original else ''}/{topic}.png")
     image = image.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
     generator = torch.manual_seed(42)
-    # import ipdb; ipdb.set_trace()
-    pipeline_image2video.enable_model_cpu_offload() #if you can use gpu
+    
     frames = pipeline_image2video(image, decode_chunk_size=8, generator=generator).frames[0]
 
-    export_to_video(frames, f"{topic}.mp4", fps=7)
+    export_to_video(frames, f"{DEST_FOLDER}/{'original' if original else ''}/mp4/{topic}.mp4", fps=7)
     
-    video = VideoFileClip(f"{topic}.mp4")
-    video.write_gif(f"{topic}.gif")
+    video = VideoFileClip(f"{DEST_FOLDER}/{'original' if original else ''}/mp4/{topic}.mp4")
+    video.write_gif(f"{DEST_FOLDER}/{'original' if original else ''}/gif/{topic}.gif")
 
+def read_txt(filename: str) -> str:
+    with open(filename, 'r') as f:
+        return f.read()
 
 if __name__ == '__main__':
+    experiments = {
+        f[:-len(TXT_SUFFIX)]: read_txt(join(SOURCE_FOLDER, f)) 
+        for f in listdir(SOURCE_FOLDER) 
+        if f[-len(TXT_SUFFIX):] == TXT_SUFFIX
+    }
+    
+    # Text to Image: PNG format
     pipeline_text2image = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16).to("cuda")
+    pipeline_text2image.enable_model_cpu_offload()
+    for topic, article in tqdm(experiments.items()):
+        generate_image(article, topic, pipeline_text2image, original=True)
+        generate_image(article, topic, pipeline_text2image, original=False)
+        
+    # Image to Videos: MP4 and GIF formats
     pipeline_image2video = DiffusionPipeline.from_pretrained(
         "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16").to("cuda")
-
-    experiments = {
-        'cheetah': """
-            In Africa's vast savannah, a swift cheetah races, epitomizing nature's splendor. Its effortless sprint highlights not just its remarkable speed but also the urgent need to protect these majestic creatures and their diminishing habitats.
-        """,
-    }
-
-    for topic, article in experiments.items():
-        generate_image(article, topic, pipeline_text2image)
-        generate_video(topic, pipeline_image2video)
+    pipeline_image2video.enable_model_cpu_offload()
+    for topic, article in tqdm(experiments.items()):
+        generate_video(topic, pipeline_image2video, original=True)
+        generate_video(topic, pipeline_image2video, original=False)
